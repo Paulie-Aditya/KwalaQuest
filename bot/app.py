f7e4de3692cb2ce9535a1d3e0b3551da51c50ce2
Paulie-Aditya <paul.aditya304@gmail.com>
2025-11-05 15:30:04 +0530

    some placeholder commands, have to still add more logic

--- bot/app.py
+++ bot/app.py
@@ -14,6 +14,34 @@ bot = commands.Bot(
     command_prefix=commands.when_mentioned_or("!"),
     intents=intents)
 
+user_to_wallet_mapping = {}
+user_xp = {}
+
+@bot.command(name="register")
+async def register(ctx, *args):
+    wallet_addy = args[0]
+    user_id = ctx.author.id
+    user_to_wallet_mapping[user_id] = wallet_addy
+    # this particular wallet address has to be now tracked
+
+    ctx.send(f"<@{ctx.author.id}> has registered {wallet_addy}", ephermal = True)
+
+@bot.command(name="xp")
+async def xp(ctx):
+    user_id = ctx.author.id
+    try:
+        user_xp[user_id]
+    except KeyError:
+        user_xp[user_id] = 0
+    ctx.send(f"<@{ctx.author.id}> has {user_xp[user_id]} XP!")
+
+@bot.command(name="unregister")
+async def unregister(ctx):
+    try:
+        del user_to_wallet_mapping[ctx.author.id]
+    except KeyError:
+        pass
+    ctx.send(f'<@{ctx.author.id}> has unregistered :(')
 
 @bot.event
 async def on_ready():
---
+++ notes.md
@@ -0,0 +1,9 @@
+User performs on-chain action → Kwala detects it → triggers webhook → backend updates user XP → Discord bot announces reward
+
+| Milestone                     | Kwala Trigger        | Bot Reward            |
+| ----------------------------- | -------------------- | --------------------- |
+| 1st transaction on chain      | ERC-20 Transfer      | “Newbie Trader” badge |
+| 10 transactions of same token | Transfer event count | Level up + XP         |
+| Holds > 1000 of token X       | Balance > threshold  | Rare “Whale” role     |
+| Mints first NFT               | ERC-721 Transfer     | NFT Collector badge   |
+| Signs up through bot          | `/register` command  | On-chain address bind |
